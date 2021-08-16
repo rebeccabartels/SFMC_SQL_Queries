2be5c2e679ba14aacbca39df6db7da0276b960e0
Rebecca Bartels <rebeccakbartels@gmail.com>
2021-08-16 19:05:15 -0400

    Create deduplicationzips.sql

---
+++ deduplicationzips.sql
@@ -0,0 +1,31 @@
+Select
+x.MDP_CustomerKey, 
+x.MDP_CompanyKey,
+x.CustomerFirstName, 
+x.CustomerLastName, 
+x.Email, 
+x.CellPhone, 
+x.Address, 
+x.Address_second_line,
+x.City, 
+x.State, 
+x.Zip,
+x.RowNum
+From (
+    Select 
+    sm.MDP_CustomerKey, 
+    sm.MDP_CompanyKey,
+    cm.CustomerFirstName, 
+    cm.CustomerLastName, 
+    cm.Email, 
+    cm.CellPhone, 
+    cm.Address, 
+    cm.Address_second_line,
+    cm.City, 
+    cm.State, 
+    cm.Zip,
+    row_number() over (partition by sm.Zip order by sm.Zip desc) rowNum
+    From Customer_Master sm
+    Inner Join Customer_Data_Zip_Codes cm ON cm.Zip = sm.Zip
+) x
+where x.rowNum = 1
